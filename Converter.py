c143bfbb330af84c879c06e55322fa4f7db0ac18
Ethan Price <32579301+EmptyString1999@users.noreply.github.com>
2019-01-29 11:20:40 +0000

    Fixed bug with rotating image
    This update is should be stable but may have a few bugs with clipping the image due to how i've done it

--- Converter.py
+++ Converter.py
@@ -2,13 +2,21 @@ from __future__ import print_function
 from PIL import Image
 
 filename = "bird.jpg"
-size = (1200, 100)
+size = (300, 100)
 
 im = Image.open(filename).convert('L')
-im.resize(size).save("grayscale.png")
+im.rotate(270).transpose(Image.FLIP_LEFT_RIGHT).resize(size).save("grayscale.png")
 im.close()
+
 img = Image.open("grayscale.png")
 width, height = img.size
+img.crop((0 + width/4, 0, width - width / 4, height)).save("grayscale.png")
+img.close()
+img = Image.open("grayscale.png")
+width, height = img.size
+
+
+
 
 px = img.load()
 
